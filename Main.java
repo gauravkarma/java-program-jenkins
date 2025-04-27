8763e4e482ec701d9f01515add81deae46cf6116
gauravkarma <gaurav.karma13@gmail.com>
2025-04-27 14:18:29 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
     System.out.println("This is java prg");
     System.out.println("Output from java prgrm");
     System.out.println(new Date());
+    System.out.println("Hello");
 }
 }
